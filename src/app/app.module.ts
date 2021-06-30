faad5faf027baec041f42093b1a512f5621a3720
Roger Muguruza <rogermuguruza1999@gmail.com>
2021-06-30 05:36:24 -0500

    Primer commit del proyecto completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';;
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { VideoNewComponent } from './components/video-new/video-new.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+
+import { IdentityGuard } from './services/identity.guard';
+import { UserService } from './services/user.service';
+import { VideoEditComponent } from './components/video-edit/video-edit.component';
+import { VideoDetailComponent } from './components/video-detail/video-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    RegisterComponent,
+    LoginComponent,
+    VideoNewComponent,
+    UserEditComponent,
+    VideoEditComponent,
+    VideoDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders,
+    IdentityGuard,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
